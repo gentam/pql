8873d8ce91d4ee1b94225652c4240af810bfd0a4
Genta Mochizawa <g.mochizawa@gmail.com>
2021-08-27 23:04:31 +0900

    add WHERE

--- select_test.go
+++ select_test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func TestSelect(t *testing.T) {
-	q1 := Select().From("t").Build()
-	q2 := Select("a", "1", "now()").From("t").Build()
-	assert.Equal(t, "SELECT * FROM t", q1)
-	assert.Equal(t, "SELECT a,1,now() FROM t", q2)
+	q1 := Select("a", "1", "now()").Build()
+	q2 := Select().From("t").Build()
+	q3 := Select().From("t").Where("true").Build()
+	assert.Equal(t, "SELECT a,1,now()", q1)
+	assert.Equal(t, "SELECT * FROM t", q2)
+	assert.Equal(t, "SELECT * FROM t WHERE true", q3)
 }
