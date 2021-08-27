e357891bc29b27f0a1cbabf0131d06165ac58969
Genta Mochizawa <g.mochizawa@gmail.com>
2021-08-28 07:10:12 +0900

    add arg list

--- select_test.go
+++ select_test.go
@@ -7,10 +7,15 @@ import (
 )
 
 func TestSelect(t *testing.T) {
-	q1 := Select("a", "1", "now()").Build()
-	q2 := Select().From("t").Build()
-	q3 := Select().From("t").Where("true").Build()
+	q1, a := Select("a", "1", "now()").Build()
+	q2, _ := Select().From("t").Build()
 	assert.Equal(t, "SELECT a,1,now()", q1)
 	assert.Equal(t, "SELECT * FROM t", q2)
-	assert.Equal(t, "SELECT * FROM t WHERE true", q3)
+	assert.Empty(t, a)
+}
+
+func TestSelectWhere(t *testing.T) {
+	q1, a := Select().From("t").Where("v").Eq(1).Build()
+	assert.Equal(t, "SELECT * FROM t WHERE v=$1", q1)
+	assert.Equal(t, []interface{}{1}, a)
 }
