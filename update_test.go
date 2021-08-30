489d3b0407eb671c32066bf1cda9612ec84caf59
Genta Mochizawa <genta@mochizawa.org>
2021-08-31 00:32:31 +0900

    increase test coverage

--- update_test.go
+++ update_test.go
@@ -11,7 +11,10 @@ func TestUpdate(t *testing.T) {
 	assert.Contains(t, []string{"UPDATE t SET c1=$1,c2=$2", "UPDATE t SET c2=$1,c1=$2"}, q)
 	assert.ElementsMatch(t, []interface{}{1, 2}, a)
 
-	q, a = Update("t").Set("c1", 1).Where("c2").Eq(2).Build()
-	assert.Equal(t, "UPDATE t SET c1=$1 WHERE (c2=$2)", q)
-	assert.Equal(t, []interface{}{1, 2}, a)
+	s := Update("t").Set("c1", 1)
+	s.Where("c2").Eq(2)
+	s.WhereNot("c3").Neq(3)
+	q, a = s.Build()
+	assert.Equal(t, "UPDATE t SET c1=$1 WHERE (c2=$2) AND (NOT c3<>$3)", q)
+	assert.Equal(t, []interface{}{1, 2, 3}, a)
 }
