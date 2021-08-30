86b0218b5d6f35de89b1a8a127a6aefbcdc5a677
Genta Mochizawa <g.mochizawa@gmail.com>
2021-08-31 00:33:08 +0900

    add delete statement

---
+++ delete.go
@@ -0,0 +1,37 @@
+package pql
+
+import "strings"
+
+type DeleteStatement struct {
+	table string
+	where []*WhereCls
+}
+
+func Delete(table string) *DeleteStatement {
+	return &DeleteStatement{table: table}
+}
+
+func (ds *DeleteStatement) Build() (string, []interface{}) {
+	sb := &strings.Builder{}
+	sb.WriteString("DELETE FROM ")
+	sb.WriteString(ds.table)
+
+	var args []interface{}
+	if ds.where != nil {
+		args = buildWhere(ds.where, sb, args)
+	}
+
+	return sb.String(), args
+}
+
+func (ds *DeleteStatement) Where(col string, args ...interface{}) *WhereCls {
+	wc := &WhereCls{stmt: ds, col: col, args: args}
+	ds.where = append(ds.where, wc)
+	return wc
+}
+
+func (ds *DeleteStatement) WhereNot(col string, args ...interface{}) *WhereCls {
+	wc := &WhereCls{stmt: ds, col: "NOT " + col, args: args}
+	ds.where = append(ds.where, wc)
+	return wc
+}
