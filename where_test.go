02a17cb48f80b111a0c748055bcb61933a06f72a
Genta Mochizawa <g.mochizawa@gmail.com>
2021-09-04 00:33:03 +0900

    get stmt from WhereCls

--- where_test.go
+++ where_test.go
@@ -10,4 +10,17 @@ func TestWhere(t *testing.T) {
 	q, a := Where("c").Build()
 	assert.Equal(t, "error: cannot build detatched WHERE clause", q)
 	assert.Nil(t, a)
+
+	s := Select()
+	w := s.Where("c")
+	assert.Equal(t, s, w.SS())
+	assert.Nil(t, w.US())
+	u := Update("t")
+	w = u.Where("c")
+	assert.Equal(t, u, w.US())
+	assert.Nil(t, w.DS())
+	d := Delete("t")
+	w = d.Where("c")
+	assert.Equal(t, d, w.DS())
+	assert.Nil(t, w.SS())
 }
