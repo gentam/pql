ec9146911a476da051090d436118271328afe3c1
Genta Mochizawa <g.mochizawa@gmail.com>
2021-09-04 00:17:49 +0900

    consistency

--- delete.go
+++ delete.go
@@ -2,16 +2,16 @@ package pql
 
 import "strings"
 
-type DeleteStatement struct {
+type DeleteStmt struct {
 	table string
 	where []*WhereCls
 }
 
-func Delete(table string) *DeleteStatement {
-	return &DeleteStatement{table: table}
+func Delete(table string) *DeleteStmt {
+	return &DeleteStmt{table: table}
 }
 
-func (ds *DeleteStatement) Build() (string, []interface{}) {
+func (ds *DeleteStmt) Build() (string, []interface{}) {
 	sb := &strings.Builder{}
 	sb.WriteString("DELETE FROM ")
 	sb.WriteString(ds.table)
@@ -24,19 +24,19 @@ func (ds *DeleteStatement) Build() (string, []interface{}) {
 	return sb.String(), args
 }
 
-func (ds *DeleteStatement) Where(col string, args ...interface{}) *WhereCls {
+func (ds *DeleteStmt) Where(col string, args ...interface{}) *WhereCls {
 	wc := &WhereCls{stmt: ds, col: col, args: args}
 	ds.where = append(ds.where, wc)
 	return wc
 }
 
-func (ds *DeleteStatement) WhereNot(col string, args ...interface{}) *WhereCls {
+func (ds *DeleteStmt) WhereNot(col string, args ...interface{}) *WhereCls {
 	wc := &WhereCls{stmt: ds, col: "NOT " + col, args: args}
 	ds.where = append(ds.where, wc)
 	return wc
 }
 
-func (ds *DeleteStatement) Apply(w *WhereCls) *DeleteStatement {
+func (ds *DeleteStmt) Apply(w *WhereCls) *DeleteStmt {
 	w.root.stmt = ds
 	ds.where = append(ds.where, w.root)
 	return ds
