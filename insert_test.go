f7911a2f2f88ee530886e0c3ade0ef4f5b3a413f
Genta Mochizawa <g.mochizawa@gmail.com>
2021-09-05 12:38:40 +0900

    add RETURNING clause to insert

--- insert_test.go
+++ insert_test.go
@@ -14,4 +14,12 @@ func TestInsert(t *testing.T) {
 	q, a = Insert("t").Values(Map{"c1": 1, "c2": 2}).Build()
 	assert.Contains(t, []string{"INSERT INTO t (c1,c2) VALUES ($1,$2)", "INSERT INTO t (c2,c1) VALUES ($1,$2)"}, q)
 	assert.ElementsMatch(t, []interface{}{1, 2}, a)
+
+	q, a = Insert("t").Set("c1", 1).Set("c2", 2).
+		Returning("c1", "c2,c3").Returning("c4").Build()
+	assert.Contains(t, []string{
+		"INSERT INTO t (c1,c2) VALUES ($1,$2) RETURNING c1,c2,c3,c4",
+		"INSERT INTO t (c2,c1) VALUES ($1,$2) RETURNING c1,c2,c3,c4",
+	}, q)
+	assert.ElementsMatch(t, []interface{}{1, 2}, a)
 }
