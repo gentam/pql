69fc4ebe1021b71964e4aa744ae575cffc40e68d
Genta Mochizawa <g.mochizawa@gmail.com>
2021-09-05 12:46:32 +0900

    add RETURNING clause to update and delete

--- pql.go
+++ pql.go
@@ -1,4 +1,16 @@
 // Package pql helps build PostgreSQL queries.
 package pql
 
+import "strings"
+
 type Map map[string]interface{}
+
+func buildReturning(sb *strings.Builder, returning []string) {
+	sb.WriteString(" RETURNING ")
+	for i, col := range returning {
+		if i != 0 {
+			sb.WriteByte(',')
+		}
+		sb.WriteString(col)
+	}
+}
